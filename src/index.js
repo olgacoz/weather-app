a70c33b85994ef2b554ac2a614573a4e430072c2
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-09-02 16:40:15 +0300

    Write function for fetching weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./styles.css";
+
+const API_KEY = "2LYJU4DK9EKDVFVNJ9ZNC9RYP";
+
+async function fetchWeatherData(location) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`,
+    );
+    const weatherData = await response.json();
+    console.log(weatherData);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+fetchWeatherData("Kyrenia");
